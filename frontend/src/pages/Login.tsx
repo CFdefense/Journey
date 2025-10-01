83d18051c3561e709ea1ab8deaa1693cb5e0a4b6
nicholas-longo <nlongo1004@gmail.com>
2025-10-01 13:22:24 -0400

    hard coded a username and password that will bring to creation page on successful login. error displayed otherwise. set up place for backend validation call

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,14 +1,25 @@
 import { useState } from 'react';
+import { useNavigate } from "react-router-dom"; 
 import "./Login.css";
 
 export default function Login() {
  const [userName, setUsername] = useState(""); // react hook to make sure that variable stays changed after React re-renders (gives components memory). https://react.dev/reference/react/useState
  const [password, setPassword] = useState("");
+ const [error, setError] = useState(""); // for showing error messages
+ const navigate = useNavigate();
 
  const handleSubmit = (e: React.FormEvent) => {
       e.preventDefault(); // stops the page from refreshing 
       console.log("username: " + userName + " password: " + password)
-      // ******** this is where it will get sent to the backend
+
+      // *****this will be replaced by backend validation API call
+      if(userName === "123" && password === "123") {
+        setError("");
+        navigate("/create") // go to itinerary creation page by default
+      } else {
+        setError("Invalid username or password.")
+      }
+      
  }
  
   return (
@@ -38,6 +49,7 @@ export default function Login() {
 
           <button type="submit">Log In</button>
         </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </div>
     </div>
   );
