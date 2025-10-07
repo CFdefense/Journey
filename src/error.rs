00e5d8b1c89d2c28765f5bf66b99432fc240890c
eclipse-909 <ethieq909@gmail.com>
2025-10-07 13:24:27 -0400

    removed temporary error enum

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,3 @@
 use axum::http::StatusCode;
 
 pub type ApiResult<T> = std::result::Result<T, StatusCode>;
-
-/// Added to fix tarpaulin bug. This can be removed once other things are added to this file
-enum Error {}
