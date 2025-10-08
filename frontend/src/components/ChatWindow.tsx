8d99c870ecf8527b8357e6c6e1d6a015be50133f
nicholas-longo <nlongo1004@gmail.com>
2025-10-08 18:59:13 -0400

    added auto bot reply and file to start the side bar chat

--- frontend/src/components/ChatWindow.tsx
+++ frontend/src/components/ChatWindow.tsx
@@ -14,13 +14,20 @@ export default function ChatWindow() {
   const handleSend = (text: string) => {
     if (!text.trim()) return;
 
-    const newMessage: Message = {
-      id: Date.now(),
-      text,
-      sender: "user",
-    };
+    const userMessage: Message = {
+		id: Date.now(),
+		text,
+		sender: "user",
+ 	};
 
-    setMessages((prev) => [...prev, newMessage]);
+	// here is where API will need to be called to get the official bot reply
+	const botMessage: Message = {
+		id: Date.now() + 1, // small offset to avoid duplicate keys
+		text: "bot reply",
+		sender: "bot",
+	};
+
+    setMessages((prev) => [...prev, userMessage, botMessage]);
   };
 
   return (
