30af9576deb000f44556403fa7b2360d0b803d52
nicholas-longo <nlongo1004@gmail.com>
2025-10-09 16:42:12 -0400

    itinerary and chat box are next to each other. home.css added

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,15 @@
 import ChatWindow from "../components/ChatWindow";
 import Itinerary from "../components/Itinerary";
+import "../styles/Home.css"; // 👈 add a CSS file for layout
 
 export default function CreateItinerary() {
-   return (
-    <div>
+  return (
+    <div className="create-itinerary-page">
       <h1>Where do you plan to explore?</h1>
-      <ChatWindow />
-      <Itinerary />
+      <div className="itinerary-layout">
+        <ChatWindow />
+        <Itinerary />
+      </div>
     </div>
   );
 }
