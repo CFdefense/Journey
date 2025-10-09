677108bb7077019fb84ca517fb4dce5b1d538ec1
nicholas-longo <nlongo1004@gmail.com>
2025-10-09 16:39:03 -0400

    Broke unassignedEvents and Itinerary into separate components in order to make itinerary easier to use down the line

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import ChatWindow from "../components/ChatWindow";
+import Itinerary from "../components/Itinerary";
 
 export default function CreateItinerary() {
    return (
     <div>
       <h1>Where do you plan to explore?</h1>
       <ChatWindow />
+      <Itinerary />
     </div>
   );
 }
