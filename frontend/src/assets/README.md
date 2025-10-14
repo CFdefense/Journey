184868db3c6fe8560f6400e5f53e779fd3c8eb36
eclipse-909 <ethieq909@gmail.com>
2025-10-14 13:23:24 -0400

    created 404 and set frontend conventions

---
+++ frontend/src/assets/README.md
@@ -0,0 +1,4 @@
+# Licenses:
+### car-on-map.jpg
+* Public Domain
+* [source](https://www.pickpik.com/holidays-car-travel-route-adventure-scandinavia-95948)
---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,24 @@
+import { useNavigate } from "react-router-dom";
+import "../styles/NotFound.css";
+
+export default function NotFoundPage() {
+  const navigate = useNavigate();
+
+  return (
+      <div className="lost-tourist-wrapper">
+        <div className="lost-tourist-card">
+          <img src="./src/assets/car-on-map.jpg" alt="Lost tourist" className="tourist-image" />
+
+          <h1 className="lost-tourist-title">Uh-oh… You’ve wandered off the map!</h1>
+
+          <p className="lost-tourist-description">
+            Looks like this page isn’t on our itinerary. Don’t worry—you’re still on the map!
+          </p>
+
+          <div className="lost-tourist-buttons">
+            <button className="btn-journey" onClick={() => navigate("/home")}>Start a New Journey</button>
+          </div>
+        </div>
+      </div>
+    );
+}
