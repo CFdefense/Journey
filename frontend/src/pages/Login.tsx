a3f8be70ec8ba6857c6289c44ebf53cfeaf73ad2
CFdefense <CFdefence@gmail.com>
2025-10-17 13:48:53 -0400

    login and signup done

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import "../styles/Login.css";
 import { apiLogin } from "../api/account";
 
@@ -27,24 +27,44 @@ export default function Login() {
         <div className="login-box">
           <h1>Log in to your account</h1>
           <form onSubmit={handleSubmit}>
-            <label>
-              Email
+            <label className="sr-only" htmlFor="email">Email</label>
+            <div className="field">
+              <span className="field__icon" aria-hidden>
+                <svg width="20" height="20" viewBox="0 0 32 32" fill="currentColor">
+                  <path d="M26.249 28H5.753a.756.756 0 0 1-.75-.83C5.338 23.752 8.898 20 16 20s10.662 3.752 10.997 7.17a.756.756 0 0 1-.75.83zM16 18c-3.201 0-5.997-2.778-5.997-7.25 0-3.85 2.421-6.75 5.997-6.75s5.997 2.9 5.997 6.75c0 4.472-2.796 7.25-5.997 7.25z" fillRule="evenodd"></path>
+                </svg>
+              </span>
               <input
+                id="email"
                 type="email"
+                placeholder="Email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 required
+                autoComplete="email"
               />
-            </label>
-            <label>
-              Password
+            </div>
+
+            <label className="sr-only" htmlFor="password">Password</label>
+            <div className="field">
+              <span className="field__icon" aria-hidden>
+                <svg width="20" height="20" viewBox="0 0 64 64" fill="currentColor">
+                  <path d="M44.01 28.02h1a4.177 4.177 0 0 1 4 4.334v17.334a4.177 4.177 0 0 1-4 4.333h-26a4.177 4.177 0 0 1-4-4.333V32.354a4.177 4.177 0 0 1 4-4.333h1v-7.005c0-4.03.846-6.379 2.729-8.262 1.883-1.883 4.23-2.73 8.258-2.73h2.026c4.029 0 6.376.847 8.258 2.73 1.883 1.883 2.729 4.231 2.729 8.262zm-4-7.002c0-2.565-.539-4.06-1.737-5.258-1.198-1.198-2.691-1.737-5.255-1.737h-2.016c-2.564 0-4.057.539-5.255 1.737-1.198 1.199-1.737 2.693-1.737 5.258v7.003h16z" fillRule="evenodd"></path>
+                </svg>
+              </span>
               <input
+                id="password"
                 type="password"
+                placeholder="Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required
+                autoComplete="current-password"
               />
-            </label>
+            </div>
+            <div className="forgot-password">
+              <Link to="/forgot">Don't remember your password?</Link>
+            </div>
             <button type="submit">Log In</button>
           </form>
           {error && <p>{error}</p>}
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -91,54 +91,99 @@ export default function Signup() {
               </div>
             </div>
           </section>
-          <div className="login-box signup-form-panel">
+          <div className="signup-box signup-form-panel">
             <h1>Create your account</h1>
             <form onSubmit={handleSubmit}>
-              <label>
-                Email
+              <label className="sr-only" htmlFor="firstName">First Name</label>
+              <div className="field">
+                <span className="field__icon" aria-hidden>
+                  <svg width="20" height="20" viewBox="0 0 32 32" fill="currentColor">
+                    <path d="M26.249 28H5.753a.756.756 0 0 1-.75-.83C5.338 23.752 8.898 20 16 20s10.662 3.752 10.997 7.17a.756.756 0 0 1-.75.83zM16 18c-3.201 0-5.997-2.778-5.997-7.25 0-3.85 2.421-6.75 5.997-6.75s5.997 2.9 5.997 6.75c0 4.472-2.796 7.25-5.997 7.25z" fillRule="evenodd"></path>
+                  </svg>
+                </span>
                 <input
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </label>
-              <label>
-                First Name
-                <input
+                  id="firstName"
                   type="text"
+                  placeholder="First Name"
                   value={firstName}
                   onChange={(e) => setFirstName(e.target.value)}
                   required
+                  autoComplete="given-name"
                 />
-              </label>
-              <label>
-                Last Name
+              </div>
+
+              <label className="sr-only" htmlFor="lastName">Last Name</label>
+              <div className="field">
+                <span className="field__icon" aria-hidden>
+                  <svg width="20" height="20" viewBox="0 0 32 32" fill="currentColor">
+                    <path d="M26.249 28H5.753a.756.756 0 0 1-.75-.83C5.338 23.752 8.898 20 16 20s10.662 3.752 10.997 7.17a.756.756 0 0 1-.75.83zM16 18c-3.201 0-5.997-2.778-5.997-7.25 0-3.85 2.421-6.75 5.997-6.75s5.997 2.9 5.997 6.75c0 4.472-2.796 7.25-5.997 7.25z" fillRule="evenodd"></path>
+                  </svg>
+                </span>
                 <input
+                  id="lastName"
                   type="text"
+                  placeholder="Last Name"
                   value={lastName}
                   onChange={(e) => setLastName(e.target.value)}
                   required
+                  autoComplete="family-name"
                 />
-              </label>
-              <label>
-                Password
+              </div>
+
+              <label className="sr-only" htmlFor="email">Email</label>
+              <div className="field">
+                <span className="field__icon" aria-hidden>
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                    <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
+                  </svg>
+                </span>
                 <input
+                  id="email"
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  autoComplete="email"
+                />
+              </div>
+
+              <label className="sr-only" htmlFor="password">Password</label>
+              <div className="field">
+                <span className="field__icon" aria-hidden>
+                  <svg width="20" height="20" viewBox="0 0 64 64" fill="currentColor">
+                    <path d="M44.01 28.02h1a4.177 4.177 0 0 1 4 4.334v17.334a4.177 4.177 0 0 1-4 4.333h-26a4.177 4.177 0 0 1-4-4.333V32.354a4.177 4.177 0 0 1 4-4.333h1v-7.005c0-4.03.846-6.379 2.729-8.262 1.883-1.883 4.23-2.73 8.258-2.73h2.026c4.029 0 6.376.847 8.258 2.73 1.883 1.883 2.729 4.231 2.729 8.262zm-4-7.002c0-2.565-.539-4.06-1.737-5.258-1.198-1.198-2.691-1.737-5.255-1.737h-2.016c-2.564 0-4.057.539-5.255 1.737-1.198 1.199-1.737 2.693-1.737 5.258v7.003h16z" fillRule="evenodd"></path>
+                  </svg>
+                </span>
+                <input
+                  id="password"
                   type="password"
+                  placeholder="Password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   required
+                  autoComplete="new-password"
                 />
-              </label>
-              <label>
-                Re-Enter Password
+              </div>
+
+              <label className="sr-only" htmlFor="confirmPassword">Re-Enter Password</label>
+              <div className="field">
+                <span className="field__icon" aria-hidden>
+                  <svg width="20" height="20" viewBox="0 0 64 64" fill="currentColor">
+                    <path d="M44.01 28.02h1a4.177 4.177 0 0 1 4 4.334v17.334a4.177 4.177 0 0 1-4 4.333h-26a4.177 4.177 0 0 1-4-4.333V32.354a4.177 4.177 0 0 1 4-4.333h1v-7.005c0-4.03.846-6.379 2.729-8.262 1.883-1.883 4.23-2.73 8.258-2.73h2.026c4.029 0 6.376.847 8.258 2.73 1.883 1.883 2.729 4.231 2.729 8.262zm-4-7.002c0-2.565-.539-4.06-1.737-5.258-1.198-1.198-2.691-1.737-5.255-1.737h-2.016c-2.564 0-4.057.539-5.255 1.737-1.198 1.199-1.737 2.693-1.737 5.258v7.003h16z" fillRule="evenodd"></path>
+                  </svg>
+                </span>
                 <input
+                  id="confirmPassword"
                   type="password"
+                  placeholder="Re-Enter Password"
                   value={confirmPassword}
                   onChange={(e) => setConfirmPassword(e.target.value)}
                   required
+                  autoComplete="new-password"
                 />
-              </label>
+              </div>
+
               <button type="submit">Get Started</button>
             </form>
             {error && <p>{error}</p>}
