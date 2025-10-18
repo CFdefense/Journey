2d67f1b4bc859058824620b7fe84743d1ce87eca
nlongo1004 <112398240+nlongo1004@users.noreply.github.com>
2025-10-18 17:33:58 -0400

    needs to be an array of numbers

--- frontend/src/models/chat.ts
+++ frontend/src/models/chat.ts
@@ -8,8 +8,8 @@ export type Message = {
 }
 
 export type ChatsResponse = {
-    chat_sessions: [number]
-}
+    chat_sessions: number[];
+};
 
 export type MessagePageRequest = {
 	chat_session_id: number,
