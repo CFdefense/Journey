34ef856c1a740fdcc0b83d57d1b10c3b107072f0
eclipse-909 <ethieq909@gmail.com>
2025-10-19 15:00:39 -0400

    updated frontend models

--- frontend/src/models/itinerary.ts
+++ frontend/src/models/itinerary.ts
@@ -1,12 +1,20 @@
 export type Itinerary = {
 	id: number,
+	/// %Y-%m-%d
     start_date: string,
+    /// %Y-%m-%d
     end_date: string,
-    morning_events: [Event],
+    event_days: [EventDay],
+    chat_session_id: number | null
+}
+
+export type EventDay = {
+	morning_events: [Event],
     noon_events: [Event],
     afternoon_events: [Event],
     evening_events: [Event],
-    chat_session_id: number | null
+    /// %Y-%m-%d
+    date: string
 }
 
 export type Event = {
