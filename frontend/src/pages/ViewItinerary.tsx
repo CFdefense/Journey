6b4e8b0faa1805f8fe9a01cd1699bfafaa7785ef
Peter110203 <peterarv1102@gmail.com>
2025-10-19 21:37:54 -0400

    trying api

--- frontend/src/pages/ViewItinerary.tsx
+++ frontend/src/pages/ViewItinerary.tsx
@@ -1,10 +1,14 @@
+import { useState, useEffect } from "react";
 import Itinerary from "../components/Itinerary";
 import UnassignedEvents from "../components/UnassignedEvents";
 import type { Event } from "../components/UnassignedEvents";
 
 import "../styles/Itinerary.css";
+import { apiItineraryDetails } from "../api/itinerary";
 
 export default function ViewItineraryPage() {
+  const [itineraryData, setItineraryData] = useState<any>(null);
+
   const unassignedEvents: Event[] = [
     { id: "1", title: "Breakfast", desc: "Saxbys coffee and bagel" },
     { id: "2", title: "Meeting", desc: "Capping discussion" },
@@ -15,6 +19,16 @@ export default function ViewItineraryPage() {
     e.dataTransfer.setData("eventTitle", event.title);
     e.dataTransfer.setData("eventDesc", event.desc || "");
   };
+
+  useEffect(() => {
+    const testId = 3;
+    apiItineraryDetails(testId)
+    .then((data) => {
+      console.log("Fetched itinerary:", data);
+      setItineraryData(data);
+    })
+    .catch((err) => console.error("Error fetching itinerary", err));
+  }, []);
 
   return (
     <div className="view-page">
